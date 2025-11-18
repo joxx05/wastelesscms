d3e0198a841073196c067e754f86fb24357b4291
joxx05 <cumy1999@gmail.com>
2025-11-18 08:39:58 +0700

    start with codered

---
+++ wastelesscms/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *  # noqa
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "l$$ifmfun#5j1a7r!h((s@gkgg4*ib%4uqk#==rh=5$fk#_ynp"
+
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+WAGTAIL_CACHE = False
+
+try:
+    from .local import *  # noqa
+except ImportError:
+    pass
